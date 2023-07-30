6ebab6dc784070a6d808ddf4f1cb56f06a7e173a
elzeresdai <elzeresdai@gmail.com>
2023-07-31 00:16:09 +0300

    Added function GetHighestGradedStudents

--- internal/students/handler.go
+++ internal/students/handler.go
@@ -4,6 +4,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"net/http"
 	"schools/internal"
+	"strconv"
 )
 
 type StudentHandler struct {
@@ -18,6 +19,7 @@ func NewStudentHandler(studentService *StudentService) internal.HandlerInterface
 
 func (h *StudentHandler) Register(e *echo.Echo) {
 	e.POST("/student", h.CreateStudent)
+	e.GET("/students/highest-graded", h.GetHighestGradedStudents)
 }
 
 func (h *StudentHandler) CreateStudent(c echo.Context) error {
@@ -32,4 +34,19 @@ func (h *StudentHandler) CreateStudent(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusCreated, createdStudent.ID)
+}
+
+func (h *StudentHandler) GetHighestGradedStudents(c echo.Context) error {
+	studentsNumber := c.QueryParam("n")
+	n, err := strconv.Atoi(studentsNumber)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid value for 'students number'")
+	}
+
+	students, err := h.studentService.GetHighestGradedStudents(n)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to load highest graded students")
+	}
+
+	return c.JSON(http.StatusOK, students)
 }
